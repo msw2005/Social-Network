e9c3cdd64167d9f23820a7a7b288adfe2c3c6a2c
M Sadiq Warsi <157520386+msw2005@users.noreply.github.com>
2025-11-25 14:14:38 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,7 +1,7 @@
 from Runs import Runner
 from loading import Loader
 from printer_console import ConsolePrinter
-#gsgsgs
+#gsgs
 
 print("Social network simulator.")
 Runner(Loader(), ConsolePrinter()).run_program(input("Enter a file name for network data: "))
